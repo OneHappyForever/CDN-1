9754daa25a332fab628a438e88a86d7b0f2bff4d
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-10-07 17:04:16 +0800

    Create index.js

---
+++ assets/jump/index.js
@@ -0,0 +1,18 @@
+var titleTime;  
+function GetQueryString(name)
+{
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if(r!=null)return  unescape(r[2]); return null;
+}
+var myurl=GetQueryString("url");
+titleTime = setTimeout(function() {  
+if(myurl !=null && myurl.toString().length>1)
+{
+window.location.href=GetQueryString("url");
+}
+else
+{
+window.location.href="https://cyfan.top";
+}
+  }, 5000);

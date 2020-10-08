ee86fb660c5c39246ae874444f8da654071a0168
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-10-08 20:15:35 +0800

    Create blogjump.js

---
+++ assets/jump/blogjump.js
@@ -0,0 +1,19 @@
+        const hasAttr = (e,a) => a.some(_=> e.attr(_)!==undefined);
+        $('a').each(function() {
+          const $this = $(this);
+          if(hasAttr($this,["data-fancybox","ignore-external-link"])) return;
+          const href = $this.attr('href');
+          if (href && href.match('^((http|https|thunder|qqdl|ed2k|Flashget|qbrowser|ftp|rtsp|mms)://)')) {
+            const strs = href.split('/');
+            if (strs.length >= 3) {
+                const host = strs[2];
+                if (host !== 'blog.cyfan.top' || window.location.host) {
+                    $this.attr('href', '/jump.html?url='+href+'').attr('rel', 'external nofollow noopener noreferrer');
+                    if (true) {
+                        $this.attr('target', '_blank');
+                    }
+                }
+            }
+          }
+        });
+
